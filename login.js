95cbeab062e5003291adc6936a4aec9b39c64849
raderbryan071-png <raderbryan071@gmail.com>
2026-09-15 09:10:57 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,3 +1,6 @@
 const gameText = document.getElementById('game-text2');
 const userInput = document.getElementById('user-input2');
 const submitBtn = document.getElementById('submit-btn2');
+function print(text) {
+  gameText.innerText += text + "\n";
+  gameText.innerscrollTop = gameText.scrollHeight;
